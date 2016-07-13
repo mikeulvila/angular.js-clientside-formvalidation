d27780cc39d9033bdffb62d3f1ca464936219a7b
Mike Ulvila <mikeulvila@me.com>
2016-07-13 10:29:25 -0500

    submitting form

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 var app = angular.module('minmax', []);
 
-app.controller('MinMaxCtrl', function ($scope) {
+app.controller('MinMaxCtrl', ['$scope', '$http', function ($scope, $http) {
   $scope.formModel;
-})
+
+  $scope.onSubmit = function () {
+    $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
+      .success(function(data) {
+        console.log('success data: ', data);
+      })
+      .error(function (error) {
+         console.error('post error: ', error)
+      });
+  }
+
+}]);
