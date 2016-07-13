280b3aa54ca82a1aa80d2fd2e535cbf07fe91832
Mike Ulvila <mikeulvila@me.com>
2016-07-13 12:00:35 -0500

    add name attr to form, add angular $valid to onSubmit

--- main.js
+++ main.js
@@ -3,14 +3,18 @@ var app = angular.module('minmax', []);
 app.controller('MinMaxCtrl', ['$scope', '$http', function ($scope, $http) {
   $scope.formModel;
 
-  $scope.onSubmit = function () {
-    $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
-      .success(function(data) {
-        console.log('success data: ', data);
-      })
-      .error(function (error) {
-         console.error('post error: ', error)
-      });
-  }
+  $scope.onSubmit = function (valid) {
+    if (valid) {
+      $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
+        .success(function(data) {
+          console.log('success data: ', data);
+        })
+        .error(function (error) {
+           console.error('post error: ', error)
+        });
+    } else {
+      console.log('invalid form');
+    }
+  } // end onSubmit
 
 }]);
